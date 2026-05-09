056984c9f756dd051ab454103d82c7e3c31c3f2b
Endyakazeyno <endy89163@gmail.com>
2026-05-09 04:02:08 +0200

    Create night.js

---
+++ plugins/game/night.js
@@ -0,0 +1,33 @@
+// Night - plugin by Endy
+import fs from "fs"
+import path from "path"
+
+let handler = async (m, { conn }) => {
+
+  const audio = path.join(process.cwd(), "media", "foxa.aac")
+
+  if (!fs.existsSync(audio)) {
+    return conn.sendMessage(
+      m.chat,
+      { text: "❌ File audio non trovato" },
+      { quoted: m }
+    )
+  }
+
+  const audioBuffer = fs.readFileSync(audio)
+
+  await conn.sendMessage(
+    m.chat,
+    {
+      audio: audioBuffer,
+      mimetype: "audio/aac",
+    },
+    { quoted: m }
+  )
+}
+
+handler.help = ["night"]
+handler.tags = ["fun"]
+handler.command = ["night"]
+
+export default handler
